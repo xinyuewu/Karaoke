5fa3eb68356c2da2d1539e714e509c34e73135fc
scottwares <scottwares@labpc513.computing.dundee.ac.uk>
2015-11-27 14:25:54 +0000

    Fixed an issue with cluster connections

--- src/main/java/uk/ac/dundee/computing/aec/karaoke/lib/CassandraHosts.java
+++ src/main/java/uk/ac/dundee/computing/aec/karaoke/lib/CassandraHosts.java
@@ -1,75 +1,53 @@
-package uk.ac.dundee.computing.aec.spotify.lib;
+package uk.ac.dundee.computing.aec.karaoke.lib;
 
 import com.datastax.driver.core.*;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.ArrayList;
-import java.util.List;
+import uk.ac.dundee.computing.aec.karaoke.lib.Keyspaces;
 
-import static uk.ac.dundee.computing.aec.spotify.lib.CassandraHosts.Host;
-
-/**
- * ********************************************************
- *
- *
- * @author administrator
- *
- * Hosts are 192.168.2.10 Seed for Vagrant hosts
- *
- *
- *
- *
- */
 public final class CassandraHosts {
 
     private static Cluster cluster;
-    static String Host = "127.0.0.1";  //at least one starting point to talk to
+    static String Host = "127.0.0.1";
 
+    //constructor
     public CassandraHosts() {
-
-    }
+    }//end constructor
 
     public static String getHost() {
         return (Host);
-    }
+    }//end getHost()
 
     public static String[] getHosts(Cluster cluster) {
-
         if (cluster == null) {
             System.out.println("Creating cluster connection");
             cluster = Cluster.builder().addContactPoint(Host).build();
-        }
+        }//end if
         System.out.println("Cluster Name " + cluster.getClusterName());
         Metadata mdata = cluster.getMetadata();
         Set<Host> hosts = mdata.getAllHosts();
         String sHosts[] = new String[hosts.size()];
-
         Iterator<Host> it = hosts.iterator();
         int i = 0;
         while (it.hasNext()) {
             Host ch = it.next();
             sHosts[i] = (String) ch.getAddress().toString();
-
             System.out.println("Hosts" + ch.getAddress().toString());
             i++;
-        }
-
+        }//end while
         return sHosts;
-    }
+    }//end getHosts()
 
     public static Cluster getCluster() {
         System.out.println("getCluster");
-        cluster = Cluster.builder().withProtocolVersion(ProtocolVersion.V3)
+        cluster = Cluster.builder()
                 .addContactPoint(Host).build();
         getHosts(cluster);
         Keyspaces.SetUpKeySpaces(cluster);
-
         return cluster;
-
-    }
+    }//end getCluster()
 
     public void close() {
         cluster.close();
-    }
-
-}
+    }//end close()
+}//end class
